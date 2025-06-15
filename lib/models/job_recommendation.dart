47b130cc3a2721c9a19a829982ac4f10d2fce7ab
qkabgg <qkabggrinio@tip.edu.ph>
2025-06-15 19:27:15 +0800

    Update job_recommendation.dart

--- lib/models/job_recommendation.dart
+++ lib/models/job_recommendation.dart
@@ -1,10 +1,16 @@
 class JobRecommendation {
   final String title;
+  final String company;
+  final List<String> requiredSkills;
+  final double matchScore;
   final double matchPercentage;
   final String description;
 
   JobRecommendation({
     required this.title,
+    required this.company,
+    required this.requiredSkills,
+    required this.matchScore,
     required this.matchPercentage,
     required this.description,
   });
@@ -12,6 +18,9 @@ class JobRecommendation {
   Map<String, dynamic> toJson() {
     return {
       'title': title,
+      'company': company,
+      'requiredSkills': requiredSkills,
+      'matchScore': matchScore,
       'matchPercentage': matchPercentage,
       'description': description,
     };
@@ -20,6 +29,9 @@ class JobRecommendation {
   factory JobRecommendation.fromJson(Map<String, dynamic> json) {
     return JobRecommendation(
       title: json['title'] as String,
+      company: json['company'] as String,
+      requiredSkills: (json['requiredSkills'] as List).cast<String>(),
+      matchScore: json['matchScore'] as double,
       matchPercentage: json['matchPercentage'] as double,
       description: json['description'] as String,
     );
